9489233cfb4bbe3685f84cbee6e1a4de666e786b
tejastejale <tejastejale13@gmail.com>
2024-12-25 22:53:00 +0530

    added marker

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, Image, View } from "react-native";
 import { Block, theme } from "galio-framework";
 import Mapbox, { Camera, MapView, PointAnnotation } from "@rnmapbox/maps";
 import { requestAndroidLocationPermissions } from "@rnmapbox/maps";
+import * as Location from "expo-location";
 import tw from "twrnc";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import marker from "../assets/imgs/location-marker.png";
 
 const { width } = Dimensions.get("screen");
 
@@ -12,32 +15,42 @@ Mapbox.setAccessToken(
 );
 
 const Home = () => {
+  const [locData, setLocData] = React.useState(null);
+  const [loc, setLoc] = React.useState([73.776171, 19.996917]);
+
   useEffect(() => {
     requestPermission();
+    locationData();
   }, []);
+
+  const url = (lat, long) => {
+    let url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${long}&lon=${lat}`;
+    return url;
+  };
+
+  const locationData = () => {
+    fetch(url(73.776171, 19.996917))
+      .then((res) => res.json())
+      .then((data) => setLocData(data));
+  };
 
   const requestPermission = async () => {
     try {
-      const permission = await requestAndroidLocationPermissions();
-      if (permission) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            console.log("Success:", position);
-            alert(
-              `Current Position: ${position.coords.latitude}, ${position.coords.longitude}`
-            );
-          },
-          (error) => {
-            console.error("Error getting location:", error);
-            alert("Error getting location");
-          }
-        );
+      // Request permission to access location
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status === "granted") {
+        // Get the current position
+        const location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.High,
+        });
+        setLoc([location.coords.longitude, location.coords.latitude]);
       } else {
         alert("Location permission denied");
       }
     } catch (error) {
-      // console.error("Error requesting permissions:", error);
-      alert("Error requesting permissions");
+      console.error("Error requesting permissions or getting location:", error);
+      alert("Error while getting location, please try again later!");
     }
   };
 
@@ -45,16 +58,25 @@ const Home = () => {
     <Block flex center style={styles.home}>
       <MapView
         style={tw`h-full w-full`}
+        styleURL="mapbox://styles/mapbox/outdoors-v12"
         zoomEnabled={true}
         rotateEnabled={true}
       >
         <Camera
           zoomLevel={15}
-          centerCoordinate={[73.776171, 19.996917]}
+          centerCoordinate={loc}
           animationMode="flyTo"
-          animationDuration={5000}
+          animationDuration={3000}
+          pitch={30}
         />
-        <PointAnnotation id="marker" coordinate={[73.776171, 19.996917]} />
+        <PointAnnotation id="marker" coordinate={loc}>
+          <View>
+            <Image
+              style={styles.marker}
+              source={require("../assets/imgs/location-marker.png")}
+            />
+          </View>
+        </PointAnnotation>
       </MapView>
     </Block>
   );
@@ -63,6 +85,11 @@ const Home = () => {
 const styles = StyleSheet.create({
   home: {
     width: width,
+  },
+  marker: {
+    height: 30,
+    width: 30,
+    resizeMode: "contain",
   },
 });
 
